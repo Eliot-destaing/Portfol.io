257cf7c435c5369934030e49efc9878d193b33e9
Eliot Destaing <eliot.destaing@epita.fr>
2025-11-12 11:21:51 +0100

    mano v10

--- libs/troika-three-text.js
+++ libs/troika-three-text.js
@@ -1,3 +1,3 @@
-export * from 'https://unpkg.com/troika-three-text@0.47.2/dist/troika-three-text.esm.js?module';
-export { Text as default } from 'https://unpkg.com/troika-three-text@0.47.2/dist/troika-three-text.esm.js?module';
+export * from './troika-three-text.esm.js';
+export { Text as default } from './troika-three-text.esm.js';
 
